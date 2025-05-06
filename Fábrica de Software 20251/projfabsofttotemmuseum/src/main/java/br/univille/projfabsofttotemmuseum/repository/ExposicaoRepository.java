38b7b3b0ae8fb798b6a4215f16652a6d0827686a
LucasZultanski <127425231+LucasZultanski@users.noreply.github.com>
2025-05-06 00:18:02 -0300

    Add files via upload

---
+++ Fábrica de Software 20251/projfabsofttotemmuseum/src/main/java/br/univille/projfabsofttotemmuseum/repository/ExposicaoRepository.java
@@ -0,0 +1,11 @@
+package br.univille.projfabsofttotemmuseum.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.univille.projfabsofttotemmuseum.entity.Exposicao;
+
+@Repository
+public interface ExposicaoRepository extends JpaRepository<Exposicao, Long> {
+    // Métodos personalizados podem ser adicionados aqui, se necessário
+}
